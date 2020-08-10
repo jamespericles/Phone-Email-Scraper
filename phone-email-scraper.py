daead8f67fb24e8212532ed628c953764fefc019
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-10 13:55:23 -0400

    annotated regex expression for phone number and wrote out the area code

--- phone-email-scraper.py
+++ phone-email-scraper.py
@@ -1,7 +1,22 @@
 #!/user/bin/env python3
 
+import re, pyperclip
+
 #TODO:
     # Create REGEX FOR PHONE NUMBERS
+    re.compile(r'''
+# several forms of numbers, XXX-XXX-XXXX, XXX-XXXX,
+(XXX) XXX-XXXX, XXX-XXXX ext 12345, ext. 12345, x12345
+
+((\d\d\d) | (\(\d\d\d\)))?          #area code (optional)
+            #first separator
+            #first 3 digits
+            #separator
+            #last 4 digits
+            # extension (optional)
+
+
+''', re.VERBOSE
     # CREATE REGEX FOR EMAIL ADDRESSES
     # GET TEXT OFF CLIPBOARD
     # EXTRA EMAIL/PHONE FROM CLIPBOARD TEXT
