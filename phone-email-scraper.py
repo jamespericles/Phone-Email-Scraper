87311d992c426ee2edb7da8dd1d72e71fcbbb762
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-10 14:02:24 -0400

    wrote email regex

--- phone-email-scraper.py
+++ phone-email-scraper.py
@@ -14,10 +14,16 @@ import re, pyperclip
 \d\d\d\d                            #last 4 digits
 (((ext(\.)?\s)| x)                  # segment of extension with words, either ext(.) or x
 (\d{2,5}))?                         # segment of extension with numbers for the extension, lenght of 2-5 digits
-
+                                    #re.VERBOSE allows for white spaces and comments
 
 ''', re.VERBOSE
     # CREATE REGEX FOR EMAIL ADDRESSES
+emailRegex = re.compile(r'''
+[a-zA-Z0-9_.+-]+  #name portion
+@                 #@symbol seperating name and domain
+[a-zA-Z0-9_.+-]+  #domain portion
+''', re.VERBOS
+                            
     # GET TEXT OFF CLIPBOARD
     # EXTRA EMAIL/PHONE FROM CLIPBOARD TEXT
     # COPY EXTRACTED TEXT TO CLIPBOARD
