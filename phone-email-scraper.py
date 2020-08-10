22320e9e78b458d2ce12ccee5a6db7d12fa31722
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-10 14:09:31 -0400

    added loop function to append all found phone numbers to an empty array, created join function to remove single quotes on either side of phone number and add each to a new line

--- phone-email-scraper.py
+++ phone-email-scraper.py
@@ -6,7 +6,7 @@ import re, pyperclip
     phoneRegex = re.compile(r'''
 # several forms of numbers, XXX-XXX-XXXX, XXX-XXXX,
 (XXX) XXX-XXXX, XXX-XXXX ext 12345, ext. 12345, x12345
-
+(
 ((\d\d\d) | (\(\d\d\d\)))?          #area code (optional)
 (\s|-)                              #first separator
 \d\d\d                              #first 3 digits
@@ -14,16 +14,27 @@ import re, pyperclip
 \d\d\d\d                            #last 4 digits
 (((ext(\.)?\s)| x)                  # segment of extension with words, either ext(.) or x
 (\d{2,5}))?                         # segment of extension with numbers for the extension, lenght of 2-5 digits
-                                    #re.VERBOSE allows for white spaces and comments
+)                                   #re.VERBOSE allows for white spaces and comments
 
 ''', re.VERBOSE
-    # CREATE REGEX FOR EMAIL ADDRESSES
+# REGEX FOR EMAIL ADDRESSES
 emailRegex = re.compile(r'''
 [a-zA-Z0-9_.+-]+  #name portion
 @                 #@symbol seperating name and domain
 [a-zA-Z0-9_.+-]+  #domain portion
 ''', re.VERBOS
                             
-    # GET TEXT OFF CLIPBOARD
-    # EXTRA EMAIL/PHONE FROM CLIPBOARD TEXT
-    # COPY EXTRACTED TEXT TO CLIPBOARD
+# GET TEXT OFF CLIPBOARD
+text = pyperclip.paste()
+                        
+# EXTRACT EMAIL/PHONE FROM CLIPBOARD TEXT
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+all PhoneNumbers =[]
+for phoneNumber in extractedPhone:
+                        allPhoneNumbers.append(phoneNumber[0])
+
+# COPY EXTRACTED TEXT TO CLIPBOARD
+'\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
+pyperclip.copy(results)
